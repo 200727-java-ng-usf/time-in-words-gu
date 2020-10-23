efc3b1c56bd6c7f946355f1434a2ac3b3e1cfd16
YuanfnegGu <yuanfenggu@hotmail.com>
2020-10-23 15:26:06 -0500

    all CRUD for event done

--- src/main/java/com/revature/controllers/EventController.java
+++ src/main/java/com/revature/controllers/EventController.java
@@ -1,29 +1,76 @@
 package com.revature.controllers;
 
+import com.revature.exceptions.ResourceNotFoundException;
 import com.revature.models.Event;
 import com.revature.repos.EventRepo;
-import com.revature.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/events")
+@RequestMapping("/eventApi")
 public class EventController {
 
-    private EventService eventService;
+    private EventRepo eventRepo;
 
     @Autowired
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
+    public EventController(EventRepo eventRepo) {
+        this.eventRepo = eventRepo;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Event> getAll() {
-        return eventService.getAllEvents();
+    @GetMapping("/events")
+    public List<Event> getAllEvents() {
+        return eventRepo.findAll();
     }
+
+
+    @GetMapping("/events/{id}")
+    public ResponseEntity<Event> getEmployeeById(@PathVariable(value = "id") Integer eventId)
+            throws ResourceNotFoundException {
+        Event event = eventRepo.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id : " + eventId));
+        return ResponseEntity.ok().body(event);
+    }
+
+    @PostMapping("/events")
+    public Event createEvent(@Valid @RequestBody Event event) {
+        return eventRepo.save(event);
+    }
+
+
+    @PutMapping("/events/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable(value = "id") Integer eventId,
+                                                   @Valid @RequestBody Event eventDetails) throws ResourceNotFoundException {
+        Event event = eventRepo.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found for this id :: " + eventId));
+
+        event.setDate(eventDetails.getDate());
+        event.setEventName(eventDetails.getEventName());
+        event.setLocation(eventDetails.getLocation());
+        event.setHour(eventDetails.getHour());
+        event.setMinute(eventDetails.getMinute());
+        Event updatedEvent = eventRepo.save(event);
+        return ResponseEntity.ok(updatedEvent);
+    }
+
+    @DeleteMapping("/events/{id}")
+    public Map<String, Boolean> deleteEvent(@PathVariable(value = "id") Integer eventId)
+            throws ResourceNotFoundException {
+        Event event = eventRepo.findById(eventId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + eventId));
+
+        eventRepo.delete(event);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
 
 
 }
