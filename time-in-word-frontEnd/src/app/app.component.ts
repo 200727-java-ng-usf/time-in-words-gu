3c600813192722f790e5ea044a3f6063196dd0cd
YuanfnegGu <yuanfenggu@hotmail.com>
2020-10-27 15:12:21 -0500

    small fix

--- time-in-word-frontEnd/src/app/app.component.ts
+++ time-in-word-frontEnd/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'time-in-word-frontEnd';
+  title = 'Time-In-Words-frontEnd';
 }
